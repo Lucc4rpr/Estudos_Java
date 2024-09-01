84d2b0f4aa4058106caa038f450b9f245a36d849
Lucca <lucca07102002@gmail.com>
2024-08-31 21:40:36 -0300

    Finalizado exercicio

---
+++ src/OperadoresAritmeticos.java
@@ -0,0 +1,23 @@
+public class OperadoresAritmeticos {
+
+
+    public static void main(String args[]) {
+        operacoesMedia();
+    }
+
+
+    public static void operacoesMedia() {
+        double nota1 = 10;
+        double nota2 = 7;
+        double nota3 = 4;
+        double nota4 = 5;
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println(media);
+    }
+
+
+}
+
+
