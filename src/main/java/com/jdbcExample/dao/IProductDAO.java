5ff67a814c435da5908aad3ec4cf81680af3253e
Lucca <lucca07102002@gmail.com>
2024-09-03 18:55:48 -0300

    fix

---
+++ src/main/java/com/jdbcExample/dao/IProductDAO.java
@@ -0,0 +1,13 @@
+package main.java.com.jdbcExample.dao;
+
+import main.java.com.jdbcExample.domain.Product;
+
+import java.util.List;
+
+public interface IProductDAO {
+    Integer save(Product product) throws Exception;
+    Integer update(Product product) throws Exception;
+    Product findByCode(String code) throws Exception;
+    List<Product> findAll() throws Exception;
+    Integer delete(Product product) throws Exception;
+}
