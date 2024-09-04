0a4137e647b9e7b2a1e2ffdeb93c5dc09724212d
Lucca <lucca07102002@gmail.com>
2024-09-03 22:37:20 -0300

    Finalizado projeto 4

--- src/main/java/com/exercise/domain/repository/AcessorioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exercise.domain.repository;
-
-import com.exercise.domain.model.Acessorio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AcessorioRepository extends JpaRepository<Acessorio, Long> {
-}
